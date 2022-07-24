660bd825baa4a79f855b68a2cb99d2e5c0cb059f
Filip Iricanin <irican_f@etna-alternance.net>
2022-07-24 17:05:59 +0200

    Book app

---
+++ src/main/java/com/m2i/rest/lesson/Book.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Other/File.java to edit this template
+ */
+package com.m2i.rest.lesson;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement(name = "book")
+@JsonPropertyOrder({"name", "isbn"})
+public class Book {
+
+    @JsonProperty("book_name")
+    protected String name;
+    @JsonProperty("book_isbn")
+    protected String isbn;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+}
